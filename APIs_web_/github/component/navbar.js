54b93298f73a334e2aa5f12eea044647b879600d
Satyajit Sahu <44902527+anubis-x-ranger@users.noreply.github.com>
2022-01-07 16:49:17 +0530

    update

---
+++ APIs_web_/github/component/navbar.js
@@ -0,0 +1,22 @@
+return `<div id="navbar">
+
+<div id="logo">
+
+    <a href="">
+
+        <img src="https://avatars.githubusercontent.com/u/9919?s=200&v=4" alt="">
+
+
+    </a>
+
+
+</div>
+
+
+<div id="search">
+
+    <input type="text" id="search-input" placeholder="Search Github">
+
+</div>
+
+<div id="options">`;
